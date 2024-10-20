bf07b62247d9a030550cd23170a1445524a8ea7b
ricardooli85 <rjb.oliveira@campus.fct.unl.pt>
2024-10-20 09:27:02 +0100

    Criei class Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using tabuleiro;
+
+namespace xadrezconsole {
+
+    class Program{
+        static void Main(string[] args){
+
+        Posicao P;
+        P = new Posicao(3, 4);
+
+        Console.WriteLine(" Posicao" + P);
+        Console.ReadLine();
+
+        }
+    }
+}
